6a33ed4c58de414fdee9b5e808f36b547a31a9a9
Andisiwe Nkwana <andisiwenkwana9@gmail.com>
2023-06-22 17:10:11 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './my-list/my-list.component.html',
+  styleUrls: ['./my-list/my-list.component.css']
 })
 export class AppComponent {
   title = 'kasi-eco';
