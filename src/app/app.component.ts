e08a48c96cdeed578952582f16817685809ea612
unknown <Liya Mabona>
2023-06-22 17:08:59 +0200

    Cohort-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
 })
 export class AppComponent {
   title = 'kasi-eco';
