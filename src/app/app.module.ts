aabec3f765c48c5c9e456284570a863d52614567
Andisiwe Nkwana <andisiwenkwana9@gmail.com>
2023-06-22 14:20:36 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreditHistoryComponent } from './credit-history/credit-history.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { PaymentPortalComponent } from './payment-portal/payment-portal.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HelpCenterComponent } from './help-center/help-center.component';
+import { MyListComponent } from './my-list/my-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreditHistoryComponent,
+    SignupComponent,
+    LoginComponent,
+    PaymentPortalComponent,
+    HomePageComponent,
+    HelpCenterComponent,
+    MyListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/credit-history/credit-history.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-credit-history',
+  templateUrl: './credit-history.component.html',
+  styleUrls: ['./credit-history.component.css']
+})
+export class CreditHistoryComponent {
+
+}
